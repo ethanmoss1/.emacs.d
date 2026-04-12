fc05fc1e45e030fc69f9e57165b6e05b57a87f10
Ethan Moss <cywinskimoss@gmail.com>
2026-04-12 07:39:31 +0100

    Updated Ignore and added modules back
    Readded many of the modules that I use frequently.
    added more ignore items

--- modules-list.el
+++ modules-list.el
@@ -13,24 +13,81 @@
    "embark"
    "helpful"
 
+   ;; -- Inbuffer Completion --
+   "corfu"
+   "cape"
+
    ;; -- Themeing Modules --
    "modus-themes"
    "nerd-icons"
    "modeline"
 
+   ;; -- Org Modules --
+   "org"
+
+   ; Agenda relatec
+   "org-contrib"
+   "org-protocol"
+   "org-export-icalendar"
+   "org-gtasks"
+
+   ; Links
+   "org-link-man"
+   "org-link-rfc"
+
+   ; Personal Knowledge Management
+   "org-roam"
+   "org-roam-timestamps"
+   "org-contacts"
+
+   ; Prettifying
+   "org-fragtog"
+   "org-appear"
+   "adaptive-wrap"
+   "olivetti"
+
+   ; org tools
+   "el-easydraw"
+
+   ;; -- Bibliography related packages --
+   ;; "citar"
+   ;; "citar-org-roam"
+   ;; "zotra"
+   ;; "pdf-tools"
+
+   ;; -- News and Email --
+   ;; "elfeed"
+   ;; "notmuch"
+   ;; "ntfy"
+
    ;; -- Programming Modules --
-   "rainbow"
    "eglot"
+
    "nix"
+   "python"
+   "c"
+
+   "dockerfile" ; syntax for dockerfiles
+
+   "rainbow"
 
    ;; -- Other Packages --
    "magit"
+   "envrc"
    "eat"
+
+   ;; -- Editing modules --
+   "programming"
+   "vundo"         ;; Visual undo history
+   "hl-todo"
+   ;; "adaptive-wrap" ;; Emacs lisp
+   ;; "jinx"
+   ;; "aggressive-indent"
+   ;; "markdown"
+
 
    ;; -- Builtin modules --
    "dired"
-
-   ;; -- Other Builtin modules --
    "keybindings"
    "repeat"
    "c-code"
